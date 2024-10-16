e66451c261d1206ae2f9be75cbd33aba7c4760d9
Noah <lorians995@gmail.com>
2024-10-16 11:04:02 +0200

    unnecessary planets removed

--- app/destination/page.js
+++ app/destination/page.js
@@ -125,41 +125,6 @@ export const Destinations = () => {
               />
             );
           })}
-
-          <div className={styles.planetCard}>
-            <img
-              className={styles.planetThumbnail}
-              src="/destination/image-europa.png"
-              alt=""
-            />
-            <div className={styles.planetDescription}>
-              <h2>EUROPA {isPlanetSelected ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button
-              className="roundButton"
-              onClick={() => onAddOrRemovePlanet("Pluto", 0)}
-            >
-              {isPlanetSelected ? "REMOVE" : "ADD PLANET"}
-            </button>
-          </div>
-          <div className={styles.planetCard}>
-            <img
-              className={styles.planetThumbnail}
-              src="/destination/image-europa.png"
-              alt=""
-            />
-            <div className={styles.planetDescription}>
-              <h2>EUROPA {isPlanetSelected ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button
-              className="roundButton"
-              onClick={() => onAddOrRemovePlanet("Pluto", 0)}
-            >
-              {isPlanetSelected ? "REMOVE" : "ADD PLANET"}
-            </button>
-          </div>
         </section>
       </main>
     </div>
