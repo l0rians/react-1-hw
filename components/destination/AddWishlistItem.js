1faf0cab39a75862599902129c728e9fa8f2f779
Noah <lorians995@gmail.com>
2024-10-23 18:34:40 +0200

    Final implementation of homework x2

--- components/destination/AddWishlistItem.js
+++ components/destination/AddWishlistItem.js
@@ -1,38 +1,66 @@
 "use client";
 
-import { useState } from 'react';
-import styles from './destination.module.css';
+import { useState } from "react";
+import styles from "./destination.module.css";
 
-export const AddWishlistItem = ({
-  onAddWishlistItem,
-}) => {
-  const [thumbnail, onThumbnailChange] = useState('/destination/image-europa.png');
-  // TASK - React 1 week 3
-  // 1. Add a useState for the handling the <input id="customWishlist" type="text" />
-  // 2. Connect the onThumbnailChange to the <select>
+export const AddWishlistItem = ({ onAddWishlistItem }) => {
+  const [thumbnail, onThumbnailChange] = useState(
+    "/destination/image-europa.png"
+  );
+
+  console.log("Thumbnail urL:", thumbnail);
+  const [wishlistItem, setWishlistItem] = useState("");
+
+  const handleThumbnailChange = (event) => {
+    onThumbnailChange(event.target.value);
+  };
+
+  const handleWishlistItemChange = (event) => {
+    setWishlistItem(event.target.value);
+  };
 
   const onAddItemPressed = () => {
-    // TASK - React 1 week 3
-    // implement this function
-    // Clear the <input/> field on button press
-    // pass the thumbnail and the name from the input to the onAddWishlistItem function
-    // call the onAddWishlistItem here
-  }
+    const finalThumbnail =
+      thumbnail === "custom" ? "/destination/image-europa.png" : thumbnail;
+    if (wishlistItem) {
+      console.log("Adding wishlist item:", {
+        name: wishlistItem,
+        thumbnail: finalThumbnail,
+      });
+      onAddWishlistItem({ name: wishlistItem, thumbnail: finalThumbnail });
 
+      setWishlistItem("");
+      onThumbnailChange("");
+    }
+  };
 
   return (
     <div className={styles.addWishlistItem}>
       <p>Add custom planet to wishlist</p>
+
       <label htmlFor="customWishlist">Wishlist item name</label>
-      <input id="customWishlist" type="text" />
+      <input
+        id="customWishlist"
+        type="text"
+        value={wishlistItem}
+        onChange={handleWishlistItemChange}
+      />
       <label htmlFor="customWishlistThumbnail">Wishlist item thumbnail</label>
-      <select id="customWishlistThumbnail" >
+      <select
+        id="customWishlistThumbnail"
+        value={thumbnail}
+        onChange={handleThumbnailChange}
+      >
         <option value="/destination/image-europa.png">EUROPA</option>
         <option value="/destination/image-mars.png">MARS</option>
         <option value="/destination/image-moon.png">MOON</option>
         <option value="/destination/image-titan.png">TITAN</option>
+        <option value="custom">CUSTOM PLANET</option>
       </select>
-      <button>ADD CUSTOM</button>
+
+      <button onClick={onAddItemPressed}>ADD CUSTOM</button>
     </div>
   );
 };
+
+export default AddWishlistItem;
